684bba3b8c1fb8b68dd4030cc8f76de3325d5686
Boldizsar Mezei <boldizsar.a.mezei@toptal.com>
2022-06-09 01:12:41 +0200

    Fixes

--- packages/firestore/test/setup.ts
+++ packages/firestore/test/setup.ts
@@ -1,12 +1,9 @@
 import * as admin from "firebase-admin";
-import test from "firebase-functions-test";
 import Soonaverse from "../src/Soonaverse";
 import serviceAccount from "../test-service-account-key.json";
 
 export const projectId = "soonaverse-dev";
 process.env.GCLOUD_PROJECT = projectId;
-
-export const testEnv = test({ projectId });
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount as any),
